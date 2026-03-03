78623d5e18cfe683514ff58fb85afc71848c491f
antdev <237216263+yumesha@users.noreply.github.com>
2026-03-03 12:39:49 +0800

    f

--- flake.nix
+++ flake.nix
@@ -1,13 +1,6 @@
 {
   description = "nix-openclaw: declarative OpenClaw packaging";
 
-  nixConfig = {
-    extra-substituters = [ "https://cache.garnix.io" ];
-    extra-trusted-public-keys = [
-      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
-    ];
-  };
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
