f04dc86dcc64c8300d0f23805e299565cc8351ac
antdev <237216263+yumesha@users.noreply.github.com>
2026-03-06 19:15:41 +0800

    f

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
     flake-utils.url = "github:numtide/flake-utils";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    nix-steipete-tools.url = "github:yumesha/nix-steipete-tools";
+    nix-steipete-tools.url = "git+ssh://git@github.com/yumesha/nix-steipete-tools.git";
   };
 
   outputs = { self, nixpkgs, flake-utils, home-manager, nix-steipete-tools }:
