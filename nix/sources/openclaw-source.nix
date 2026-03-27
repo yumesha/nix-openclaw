7af57b80e36e11a058de69ff828d39773fc46e62
antdev <237216263+yumesha@users.noreply.github.com>
2026-03-28 04:47:57 +0800

    fix: update hash for yumesha/openclaw v2026.3.10

--- nix/sources/openclaw-source.nix
+++ nix/sources/openclaw-source.nix
@@ -4,6 +4,6 @@
   owner = "yumesha";
   repo = "openclaw";
   rev = "v2026.3.10";
-  hash = "sha256-RClxePc5JZNWJ/f3U5Uy0UCvh3Pw3LOVjoqQvjr3wCc=";
+  hash = "sha256-USC67TEGWjsnV9GiNDF/BoIjMdH7TzGF4aDt2wCEDEk=";
   pnpmDepsHash = "sha256-CpgulfZfN1yHXpXBR1wvsSaME8y9ZX+dQQ4JJjp3PRs=";
 }
